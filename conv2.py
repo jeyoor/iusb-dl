4807c16d92c116f04973a0e76c0e3206b60c56de
Jeyan Oorjitham <jeyoor@gmx.com>
2019-09-17 07:23:53 -0400

    switching to conv2 file

---
+++ conv2.py
@@ -0,0 +1,110 @@
+
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam,SGD
+from keras.callbacks import ModelCheckpoint,CSVLogger
+
+
+DATASET_PATH  = './dataset'
+IMAGE_SIZE    = (224, 224)
+#IMAGE_SIZE    = (32, 32)
+NUM_CLASSES   = 2
+BATCH_SIZE    = 32  # try reducing batch size or freeze more layers if your GPU runs out of memory
+NUM_EPOCHS    = 100
+WEIGHTS_FINAL = 'model-resnet50-final.h5'
+
+
+train_datagen = ImageDataGenerator( rescale=1.0 / 255.0,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                    channel_shift_range=10,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+train_batches = train_datagen.flow_from_directory(DATASET_PATH + '/train',
+                                                  target_size=IMAGE_SIZE,
+                                                  interpolation='bicubic',
+                                                  class_mode='categorical',
+                                                  shuffle=True,
+                                                  batch_size=BATCH_SIZE)
+
+valid_datagen = ImageDataGenerator(rescale=1.0/255.0)
+valid_batches = valid_datagen.flow_from_directory(DATASET_PATH + '/valid',
+                                                  target_size=IMAGE_SIZE,
+                                                  interpolation='bicubic',
+                                                  class_mode='categorical',
+                                                  shuffle=False,
+                                                  batch_size=BATCH_SIZE)
+
+#Instantiate a Keras model object
+model = Sequential()
+
+#Now make our first convolutional layer, 32 filters, 3x3, default stride and padding
+model.add(Conv2D(32,(3,3),input_shape=(IMAGE_SIZE[0],IMAGE_SIZE[1],3),activation='relu'))
+
+#Do a max pooling, increase number of filters, but reduce feature map size
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+#And another convolution/pooling pair
+model.add(Conv2D(64,(3,3),activation='relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+#Finally a convolutional layer
+model.add(Conv2D(128,(3,3),activation='relu'))
+
+#Make output of prior convolutional layer 1D for Dense layers
+model.add(Flatten())
+
+#Dense layers form the classifier using input from convolutional layers
+model.add(Dense(units=32,activation='sigmoid')) #Could be 'relu' activation
+model.add(Dense(units=2,activation='softmax'))  #Softmax represents outputs as probabilites
+
+#Use Adam optimizer (instead of plain SGD), set learning rate to explore.
+adam = Adam(lr=.001)
+
+#Compile model
+model.compile(optimizer = adam, loss='categorical_crossentropy', metrics=['accuracy'])
+
+#Print layers for resulting model
+model.summary()
+
+#Log training data into csv file
+csv_logger = CSVLogger(filename="log.csv")
+cblist = [csv_logger]
+
+# train the model
+model.fit_generator(train_batches,
+                        steps_per_epoch = train_batches.samples // BATCH_SIZE,
+                        validation_data = valid_batches,
+                        validation_steps = valid_batches.samples // BATCH_SIZE,
+                        epochs = NUM_EPOCHS,
+			callbacks=cblist)
+
+# save trained model and weights
+model.save(WEIGHTS_FINAL)
+
+#Generators adapted from:
+#MIT License
+#
+#Copyright (c) 2018 JK Jung
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
